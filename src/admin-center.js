12a4c4ec1af708d2f6d47e7b033fcefbc1f49586
你的名字或昵称 <>
2018-02-09 19:13:49 +0800

    做完店铺详情交互

--- src/admin-center.js
+++ src/admin-center.js
@@ -41,10 +41,11 @@ $(function () {
                 // 古玩城更改时获取相应店铺数据
                 $("#curio").change(function () {
                     curioId = $(this).find("option:selected").val();
-                    console.log(curioId,'古玩城id222');
+                    // console.log(curioId,'古玩城id222');
                     // $("#shopList").remove();
                     requestShopData(curioId);
                 });
+
             }
         })
     };
@@ -71,10 +72,117 @@ $(function () {
                 console.log(info,"古玩城下店铺");
                 // 调用模板引擎处理店铺列表
                 var shopsHtml = template("myShopList",info);
-                console.log(shopsHtml,'店铺列表模板');
-                // $("#shopList").html(shopsHtml);
-                document.getElementById("shopList").innerHTML = shopsHtml;
+                $("#shopList").html(shopsHtml);
+
+                $.each(info.data,function(index,value){
+                    if(value.recommoned == 0){
+                        $('#shopList').on('click','.recommend',function () {
+                            var id = $(this).attr('data-id');
+                            console.log(id,"店铺id");
+                            $.ajax({
+                                url:"http://120.27.226.156:8080/roo-mobile-web/partner/shop/"+id+"/recommoned",
+                                type:"PUT",
+                                dataType: "JSON",
+                                contentType: "application/json;charset=utf-8",
+                                headers: {
+                                    'Authorization': Authorization,
+                                    'cityId': cityId
+                                },
+                                success:function(info){
+                                    console.log(info,"推荐店铺");
+                                    window.location.reload();
+                                }
+                            })
+                        })
+                    };
+                    if(value.recommoned == 1){
+                        $('.recommend').text('取消推荐').css('color','#999');
+                        $('#shopList').on('click','.recommend',function () {
+                            var id = $(this).attr('data-id');
+                            console.log(id,"店铺id");
+                            $.ajax({
+                                url:"http://120.27.226.156:8080/roo-mobile-web/partner/shop/"+id+"/unrecommoned",
+                                type:"PUT",
+                                dataType: "JSON",
+                                contentType: "application/json;charset=utf-8",
+                                headers: {
+                                    'Authorization': Authorization,
+                                    'cityId': cityId
+                                },
+                                success:function(info){
+                                    console.log(info,"取消推荐");
+                                    window.location.reload();
+                                }
+                            })
+                        })
+                    };
+                    if(value.shopState == 0){
+                        // c89355
+                        $('.closed').text('开启店铺').css('color','#666');
+                        $('#shopList').on('click','.closed',function () {
+                            var id = $(this).attr('data-id');
+                            console.log(id,"店铺id");
+                            $.ajax({
+                                url:"http://120.27.226.156:8080/roo-mobile-web/partner/shop/"+id+"/open",
+                                type:"PUT",
+                                dataType: "JSON",
+                                contentType: "application/json;charset=utf-8",
+                                headers: {
+                                    'Authorization': Authorization,
+                                    'cityId': cityId
+                                },
+                                success:function(info){
+                                    console.log(info,"开启店铺");
+                                    window.location.reload();
+                                }
+                            })
+                        });
+                    };
+                    if(value.shopState == 1){
+                        $('#shopList').on('click','.closed',function () {
+                            var id = $(this).attr('data-id');
+                            console.log(id,"店铺id");
+                            $.ajax({
+                                url:"http://120.27.226.156:8080/roo-mobile-web/partner/shop/"+id+"/unopen",
+                                type:"PUT",
+                                dataType: "JSON",
+                                contentType: "application/json;charset=utf-8",
+                                headers: {
+                                    'Authorization': Authorization,
+                                    'cityId': cityId
+                                },
+                                success:function(info){
+                                    console.log(info,"关闭店铺");
+                                    window.location.reload();
+                                }
+                            })
+                        });
+                    }
+                });
             }
         })
     }
+
+    $('#shopList').on('click','.details',function () {
+        var id = $(this).attr('data-id');
+        console.log(id,"店铺id");
+        $.ajax({
+            url:"http://120.27.226.156:8080/roo-mobile-web/partner/shop/"+id,
+            type:"GET",
+            dataType: "JSON",
+            contentType: "application/json;charset=utf-8",
+            headers: {
+                'Authorization': Authorization,
+                'cityId': cityId
+            },
+            success:function(info){
+                console.log(info,"店铺详情");
+                $('#shops').css('display','none');
+                $('#shopDetail').css('display','block');
+                // 调用模板引擎处理店铺详情
+                var shopsDetailHtml = template("myShopDetail",info.data);
+                $(".content").html(shopsDetailHtml);
+            }
+        })
+    })
 })
